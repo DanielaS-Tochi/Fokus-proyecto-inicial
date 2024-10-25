7ec779f7627268e5af0a9bcadde0748094abb4d7
DanielaS-Tochi <danielastochi@gmail.com>
2024-10-25 20:04:42 -0300

    html lang="es" corrected (before lang="esp"

--- script.js
+++ script.js
@@ -4,19 +4,40 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque')
 const botonLargo = document.querySelector('.app__card-button--largo')
 const banner = document.querySelector('.app__image')
 
+//Esto sería una versión larga y repetitiva, por eso vamos a crear una función.
+// botonCorto.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'descanso-corto')
+//     banner.setAttribute('src', './imagenes/descanso-corto.png')
+// })
+
+// botonEnfoque.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'enfoque')
+//     banner.setAttribute('src', './imagenes/enfoque.png')
+// })
+
+
+// botonLargo.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'descanso-largo')
+//     banner.setAttribute('src', './imagenes/descanso-largo.png')
+// })
+
+
+//Aquí hacemos la versión corta con una función:
 
 botonCorto.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-corto')
-    banner.setAttribute('src', './imagenes/descanso-corto.png')
+    cambiarContexto('descanso-corto')
 })
 
 botonEnfoque.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'enfoque')
-    banner.setAttribute('src', './imagenes/enfoque.png')
+    cambiarContexto('enfoque')
 })
 
 
 botonLargo.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-largo')
-    banner.setAttribute('src', './imagenes/descanso-largo.png')
+    cambiarContexto('descanso-largo')
 })
+
+function cambiarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagenes/${contexto}.png`)
+} 
