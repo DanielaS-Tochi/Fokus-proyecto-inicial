86917f547a0a4b5c194252bdb1ded2414269aa20
DanielaS-Tochi <danielastochi@gmail.com>
2024-10-30 21:31:02 -0300

    innerHTML

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botonCorto = document.querySelector('.app__card-button--corto')
 const botonEnfoque = document.querySelector('.app__card-button--enfoque')
 const botonLargo = document.querySelector('.app__card-button--largo')
 const banner = document.querySelector('.app__image')
-
+const titulo = document.querySelector('.app__title')
 //Esto sería una versión larga y repetitiva, por eso vamos a crear una función.
 // botonCorto.addEventListener('click', () => {
 //     html.setAttribute('data-contexto', 'descanso-corto')
@@ -40,4 +40,18 @@ botonLargo.addEventListener('click', () => {
 function cambiarContexto(contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagenes/${contexto}.png`)
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `¿Qué tal tomar un respiro? <strong class="app__title-strong">¡Has una pausa corta!</strong>`
+            break;
+        case "descanso-largo":
+            titulo.innerHTML = `¡Hora de volver a la superficie! <strong class="app__title-strong"> Has una pausa larga</strong>`
+
+        default:
+            break;
+    }
 } 
